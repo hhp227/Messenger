4f778ff0c7ead347a52fbfb41bfe67be7704a549
hhp227 <hong227@naver.com>
2019-12-15 23:18:02 +0900

    2019.12.15 Commit

--- app/src/main/java/com/hhp227/messenger/MessageActivity.java
+++ app/src/main/java/com/hhp227/messenger/MessageActivity.java
@@ -5,7 +5,6 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.*;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
